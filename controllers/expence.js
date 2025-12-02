a987bdbbc04fb5e6b159628ed848493ba74b5613
anseyyy <ahamedansil43@gmail.com>
2025-12-02 11:08:25 +0530

    update

--- controllers/expence.js
+++ controllers/expence.js
@@ -1,25 +1,22 @@
-const ExpenceSchema = require ("../models/expenceModel");
+const ExpenceSchema = require("../models/expenceModel");
 
 exports.addExpence = async (req, res) => {
   const { title, amount, category, description, date } = req.body;
-  
-
 
   const income = ExpenceSchema({
     title,
     amount,
     category,
     description,
-    date
-  })
+    date,
+  });
   try {
-
     console.log("inside try");
-    
+
     if (!title || !category || !description || !date) {
       return res.status(400).json({ message: "all feilds are required" });
     }
-    if (!amount === "number" || amount <= 0) {
+    if (typeof amount !== "number" || amount <= 0) {
       return res.status(400).json({ message: "Enter correct amount" });
     }
     await income.save();
@@ -28,26 +25,24 @@ exports.addExpence = async (req, res) => {
     res.status(500).json({ message: "server error" });
   }
   console.log(income);
-  
-}
+};
 
-exports.getExpence = async (req,res)=>{
-    try{
-        const incomes = await ExpenceSchema.find().sort({createAt:-1})
-        res.status(200).json(incomes)
-    } catch(error){
-        res.status(500).json({ message: "server error" });
-    }
-}
+exports.getExpence = async (req, res) => {
+  try {
+    const expenses = await ExpenceSchema.find().sort({ createdAt: -1 });
+    res.status(200).json(expenses);
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+};
 
-
-exports.deleteExpence = async (req,res)=>{
-    const {id} = req.params;
-    ExpenceSchema.findOneAndDelete(id)
+exports.deleteExpence = async (req, res) => {
+  const { id } = req.params;
+  ExpenceSchema.findByIdAndDelete(id)
     .then((income) => {
-        res.status(200).json({message:'Expence Deleted '})
+      res.status(200).json({ message: "Expence Deleted " });
     })
     .catch((err) => {
-        res.status(500).json({message:'Server Error '})
-    })
-}
+      res.status(500).json({ message: "Server Error " });
+    });
+};
--- controllers/income.js
+++ controllers/income.js
@@ -2,24 +2,21 @@ const IncomeSchema = require("../models/incomeModel");
 
 exports.addIncome = async (req, res) => {
   const { title, amount, category, description, date } = req.body;
-  
-
 
   const income = IncomeSchema({
     title,
     amount,
     category,
     description,
-    date
-  })
+    date,
+  });
   try {
-
     console.log("inside try");
-    
+
     if (!title || !category || !description || !date) {
       return res.status(400).json({ message: "all feilds are required" });
     }
-    if (!amount === "number" || amount <= 0) {
+    if (typeof amount !== "number" || amount <= 0) {
       return res.status(400).json({ message: "Enter correct amount" });
     }
     await income.save();
@@ -28,26 +25,24 @@ exports.addIncome = async (req, res) => {
     res.status(500).json({ message: "server error" });
   }
   console.log(income);
-  
-}
+};
 
-exports.getIncomes = async (req,res)=>{
-    try{
-        const incomes = await IncomeSchema.find().sort({createAt:-1})
-        res.status(200).json(incomes)
-    } catch(error){
-        res.status(500).json({ message: "server error" });
-    }
-}
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({ createdAt: -1 });
+    res.status(200).json(incomes);
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+};
 
-
-exports.deleteIncome = async (req,res)=>{
-    const {id} = req.params;
-    IncomeSchema.findOneAndDelete(id)
+exports.deleteIncome = async (req, res) => {
+  const { id } = req.params;
+  IncomeSchema.findByIdAndDelete(id)
     .then((income) => {
-        res.status(200).json({message:'Income Deleted '})
+      res.status(200).json({ message: "Income Deleted " });
     })
     .catch((err) => {
-        res.status(500).json({message:'Server Error '})
-    })
-}
+      res.status(500).json({ message: "Server Error " });
+    });
+};
